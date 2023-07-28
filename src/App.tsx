5ed3677390d4af0e11fc29e5711d4ca30cd775f8
Stian Jensen <me@stianj.com>
2023-07-28 15:52:01 +0200

    Make rounds and teams collapsible

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import "./App.css";
 import players from "./data/players.json";
 import league from "./data/league.json";
+import { Collapse } from "reactstrap";
+import { useState } from "react";
 
 const teams = league.teams.sort((a, b) => b.score.total - a.score.total);
 
 const TeamRound = ({
   slug,
   round,
+  initialIsOpen,
 }: {
   slug: string;
   round: typeof teams[0]["results"]["round-1"];
+  initialIsOpen?: boolean;
 }) => {
+  const [isOpen, setIsOpen] = useState(initialIsOpen ?? false);
+
   return (
     <div className="card">
-      <strong className="card-header d-flex">
+      <strong
+        className="card-header d-flex"
+        onClick={() => {
+          setIsOpen((previous) => !previous);
+        }}
+        style={{ cursor: "pointer" }}
+      >
         <div className="flex-fill">{slug}</div>
         <div className="badge rounded-pill text-bg-secondary">
           {round.score}
         </div>
       </strong>
-      <div className="card-body">
+      <Collapse isOpen={isOpen} className="card-body">
         {round.players.map((player) => {
           const playerInfo = players.find(
             (it) => it.playerId === player.playerId
@@ -52,25 +64,46 @@ const TeamRound = ({
             );
           }
         })}
-      </div>
+      </Collapse>
     </div>
   );
 };
 
-const Team = ({ team }: { team: typeof teams[0] }) => {
+const Team = ({
+  team,
+  className,
+}: {
+  team: typeof teams[0];
+  className?: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="mb-4">
-      <h3 className="d-flex">
-        <div>{team.teamName}</div>
+    <div className={className}>
+      <h3
+        className="d-flex align-items-center"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          setIsOpen((previous) => !previous);
+        }}
+      >
+        <div className="flex-fill">{team.teamName}</div>
         <div className="badge rounded-pill text-bg-secondary ms-3">
           {team.score.total}
         </div>
       </h3>
-      <div className="d-flex text-start gap-3">
-        {Object.entries(team.results).map(([slug, round]) => (
-          <TeamRound key={slug} slug={slug} round={round} />
-        ))}
-      </div>
+      <Collapse isOpen={isOpen}>
+        <div className="d-flex flex-column gap-3">
+          {Object.entries(team.results).map(([slug, round], index, results) => (
+            <TeamRound
+              key={slug}
+              slug={slug}
+              round={round}
+              initialIsOpen={index === results.length - 1}
+            />
+          ))}
+        </div>
+      </Collapse>
     </div>
   );
 };
@@ -82,9 +115,9 @@ function App() {
   return (
     <div className="container">
       <h1>Teams</h1>
-      <div>
+      <div className="list-group">
         {teams.map((team) => (
-          <Team key={team.teamName} team={team} />
+          <Team key={team.teamName} team={team} className="list-group-item" />
         ))}
       </div>
     </div>
