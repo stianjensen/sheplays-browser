af8f76b83a6b91a44bc144597f677e4d83880368
Stian Jensen <me@stianj.com>
2023-07-28 16:10:53 +0200

    Iterate on overall layout
    We're on a custom domain now!

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sheplays-browser/", // Temporary, before domain is in place
   plugins: [react()],
 });
