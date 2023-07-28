253f38daeb7c2501bd79c671d083d05e060b89d7
Stian Jensen <me@stianj.com>
2023-07-28 15:42:04 +0200

    Split into components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,77 @@ import league from "./data/league.json";
 
 const teams = league.teams.sort((a, b) => b.score.total - a.score.total);
 
+const TeamRound = ({
+  slug,
+  round,
+}: {
+  slug: string;
+  round: typeof teams[0]["results"]["round-1"];
+}) => {
+  return (
+    <div className="card">
+      <strong className="card-header d-flex">
+        <div className="flex-fill">{slug}</div>
+        <div className="badge rounded-pill text-bg-secondary">
+          {round.score}
+        </div>
+      </strong>
+      <div className="card-body">
+        {round.players.map((player) => {
+          const playerInfo = players.find(
+            (it) => it.playerId === player.playerId
+          );
+          if (playerInfo) {
+            return (
+              <div className="d-flex gap-3 py-1">
+                <div
+                  className={`flex-fill ${
+                    player.played ? "" : "text-secondary"
+                  }`}
+                >
+                  {playerInfo.name}
+                </div>
+                {"points" in player && (
+                  <div className="badge rounded-pill text-bg-primary">
+                    {player.points as any}
+                  </div>
+                )}
+                {player.isCaptain && (
+                  <div className="badge rounded-pill text-bg-success">x 2</div>
+                )}
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                <em>Player info missing</em>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Team = ({ team }: { team: typeof teams[0] }) => {
+  return (
+    <div className="mb-4">
+      <h3 className="d-flex">
+        <div>{team.teamName}</div>
+        <div className="badge rounded-pill text-bg-secondary ms-3">
+          {team.score.total}
+        </div>
+      </h3>
+      <div className="d-flex text-start gap-3">
+        {Object.entries(team.results).map(([slug, round]) => (
+          <TeamRound key={slug} slug={slug} round={round} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 function App() {
   console.log("players", players);
   console.log("league", league);
@@ -13,62 +84,7 @@ function App() {
       <h1>Teams</h1>
       <div>
         {teams.map((team) => (
-          <div key={team.teamName} className="mb-4">
-            <h3 className="d-flex">
-              <div>{team.teamName}</div>
-              <div className="badge rounded-pill text-bg-secondary ms-3">
-                {team.score.total}
-              </div>
-            </h3>
-            <div style={{ display: "flex", textAlign: "left", gap: 16 }}>
-              {Object.entries(team.results).map(([slug, round]) => (
-                <div key={slug} className="card">
-                  <strong className="card-header d-flex">
-                    <div className="flex-fill">{slug}</div>
-                    <div className="badge rounded-pill text-bg-secondary">
-                      {round.score}
-                    </div>
-                  </strong>
-                  <div className="card-body">
-                    {round.players.map((player) => {
-                      const playerInfo = players.find(
-                        (it) => it.playerId === player.playerId
-                      );
-                      if (playerInfo) {
-                        return (
-                          <div className="d-flex gap-3 py-1">
-                            <div
-                              className={`flex-fill ${
-                                player.played ? "" : "text-secondary"
-                              }`}
-                            >
-                              {playerInfo.name}
-                            </div>
-                            {"points" in player && (
-                              <div className="badge rounded-pill text-bg-primary">
-                                {player.points as any}
-                              </div>
-                            )}
-                            {player.isCaptain && (
-                              <div className="badge rounded-pill text-bg-success">
-                                x 2
-                              </div>
-                            )}
-                          </div>
-                        );
-                      } else {
-                        return (
-                          <div>
-                            <em>Player info missing</em>
-                          </div>
-                        );
-                      }
-                    })}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+          <Team key={team.teamName} team={team} />
         ))}
       </div>
     </div>
